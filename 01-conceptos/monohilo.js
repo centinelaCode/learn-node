d23ca1a60359857a9974f5c02afb4de3f2dcb422
centinela1024tb <centinela1024tb@gmail.com>
2023-01-26 13:38:21 -0600

    [ADD] Conceptos de node
    => monohilo
    => variables de entorno

---
+++ 01-conceptos/monohilo.js
@@ -0,0 +1,16 @@
+
+console.log('Hola mundo');
+
+let i = 100;
+
+setInterval(function(){
+  console.log(i)
+  i=i+100;
+
+  // if(i === 5) {
+  //   console.log('Forzamos error')
+  //   var a = 3 + z;
+  // }
+}, 1000)
+
+console.log('ultimo Segunda instruccion')
