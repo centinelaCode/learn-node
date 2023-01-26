1056db53b84234dd8d58474ffb08685159610265
centinela1024tb <centinela1024tb@gmail.com>
2023-01-26 17:55:27 -0600

    [ADD] Modulos - objetos globales

---
+++ 03-modulos/globales.js
@@ -0,0 +1,30 @@
+
+// muestra todo lo que contine el objeto global
+// console.log(global);
+// console.log(process);
+// console.log(__dirname);
+
+
+//TODO setInteval -> Ejecuta una accion cada determinado tiempo
+// let i=0;
+
+// let intervalo = setInterval( () => {
+//    console.log('Hola');
+//    if(i === 3) {
+//       // limpia el intervalo y detiene el setInterval
+//       clearInterval(intervalo);
+//    }
+//    i++;
+// }, 1000);
+
+
+
+//TODO setImmediate -> Ejecuta una accion de forma inmediata
+// setImmediate ( function() {
+//    console.log('Se ejecuta antes que nada')
+// });
+  
+
+//TODO definir variables globales
+global.miVariable = '6058741258965241df1452';
+console.log(miVariable);
