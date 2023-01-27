2818d9452e58a8bc9b021e49472b160a7e9ce9c0
centinela1024tb <centinela1024tb@gmail.com>
2023-01-26 18:14:58 -0600

    [ADD] Modulos - consola (variantes de console.xxxx)

---
+++ 03-modulos/consola.js
@@ -0,0 +1,55 @@
+//TODO Diferentes tipos de console
+
+// console.log('consol.log')
+// console.info('consol.info')
+// console.error('consol.error')
+// console.warn('consol.warn')
+// var table = [
+//    { 
+//       a: 1,
+//       b:'Z'
+//    },
+//    { 
+//       a: 3,
+//       b:'W'
+//    }
+// ];
+// console.table(table)
+
+// console.group('Conversación')
+// console.log('Hola');
+// console.group('bla')
+// console.log('Bla bla bla bla');
+// console.log('Bla bla bla bla');
+// console.log('Bla bla bla bla');
+// console.groupEnd('bla')
+// console.log('Adios')
+// console.groupEnd('Conversación')
+
+// console.log('Otra cosa')
+
+
+// function funcion1() {
+//    console.group('funcion1')
+//    console.log('Esto es de la funcion 1')
+//    console.log('Esto tambien')
+//    console.log('Esto tambien')
+//    funcion2()
+//    console.log('Hemos vuelto a la funcion 1')
+//    console.groupEnd('funcion1')
+// }
+
+// function funcion2() {
+//    console.group('funcion2')
+//    console.log('Ahora estamos en la funcion 2')
+//    console.groupEnd('funcion2')   
+// }
+// console.log('Empezamos')
+// funcion1();
+
+
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.countReset('veces')
+console.count('veces')
